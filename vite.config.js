b61450b6c863f15c2cc46a0ccb6762ce1261b793
Niver <vlader122@gmail.com>
2023-08-28 18:53:32 -0400

    añadiendo base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://niver-copa.github.io/JoseMini-ProyectoNivel2/'
+  base: 'https://niver-copa.github.io/'
 })
